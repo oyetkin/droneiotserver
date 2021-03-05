85fd3a7e421929d9a79e473b96b9243d5889b156
isConic <cncrlf@gmail.com>
2021-03-04 19:27:16 -0500

    Add a ton of functionality

--- server.py
+++ server.py
@@ -12,12 +12,25 @@ from utils.general import load_yaml
 from utils.fast import enable_cors
 
 class SensorPayload(BaseModel):
-    timestamp: int
+    timestamp: Optional[int]
     key:str
     unit:str
     value: float
     lat:Optional[float]
     lon:Optional[float]
+
+class SensorBatch(BaseModel):
+	items:List[SensorPayload]
+
+class BoundedQuery(BaseException):
+	time_range: Optional[Tuple[int, int]]
+	lat_range: Optional[Tuple[float, float]]
+	lon_range: Optional[Tuple[float, float]]
+	key: Optional[str]
+	unit: Optional[str]
+	# unit_regex: Optional[str]
+	# key_regex: Optional[str]
+
 
 path = "fake_database12.json"
 
@@ -25,13 +38,58 @@ app = FastAPI()
 enable_cors(app)
 api = CSVAPI(path = path, schema = SensorPayload)
 
+@app.post("/api/v0p1/sensor")
+async def post_sensor(payload: SensorPayload):
+	"""
+	Post sensor data.
+	"""
+
+    print(payload)
+    api.write(payload)
+    return {"status": "ok"}
+
+
+@app.post("/api/v0p1/sensor/batch")
+async def post_sensor_batch(payload: SensorBatch):
+	"""
+	Post sensor data.
+	"""
+	for item in payload.items:
+		print(payload)
+		api.write(payload)
+	return {"status": "ok"}
+
+@app.get("/api/v0p1/query/bounded")
+async def bounded_query(payload: BoundedQuery):
+#   Idea: Make ranges bounded in one direction only
+    data:List[SensorPayload] = api.get_data()
+	keys:List[str] = list(dict(data).keys())
+
+	if "time_range" in keys:
+		in_range = lambda d: d.timestamp < payload.time_range[1] && d.timestamp > payload.time_range[0] 
+		data = filter(in_range, data)
+	
+	if "lat_range" in keys:
+		in_range = lambda d: d.lat < payload.lat_range[1] && d.lat > payload.lat_range[0] 
+		data = filter(in_range, data)
+
+	if "lon_range" in keys:
+		in_range = lambda d: d.lon < payload.lon_range[1] && d.lon > payload.lon_range[0] 
+		data = filter(in_range, data)
+
+	if "key" in keys:
+		has_key = lambda d: payload.key in d  
+		data = filter(has_key, data)
+
+	if "unit" in keys"
+		has_unit = lambda d: playload.unit in d
+		data = filter(has_unit, data)	
+	
+	return list(data)
+
 @app.get("/health")
 def health():
 	return {"status":"up"}
-
-#@app.post("/health")
-#def health():
-#	 return {"status": "up"}
 
 @app.get("/")
 def home():
@@ -40,16 +98,22 @@ def home():
 	fs.close()
 	return HTMLResponse( content )
 
-@app.post("/api/v0p1/sensor")
-async def post_sensor(payload: SensorPayload):
-    print(payload)
-    api.write(payload)
-    return {"status": "ok"}
 
 @app.get("/api/v0p1/debug/get_data")
 async def debug_get_all_data():
     data:List[SensorPayload] = api.get_data()
     return data
+
+import time
+@app.get("/api/v0p1/debug/get_timestamp")
+async def debug_get_ts():
+    return {"timestamp": time.time()}
+
+import time
+@app.get("/api/v0p1/debug/get_dallas")
+async def debug_get_dallas():
+    return {"lat": 32.7767 , "lon": -96.7970}
+
 
 if __name__ == "__main__":
 	server_configs = load_yaml("configs/server_config.yaml")
